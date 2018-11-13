5776f19fba31a12d70445d9be9e3a274e6d469d9
chenqiang <1437173180@qq.com>
2018-11-13 17:05:19 +0800

    Initial commit

---
+++ e3-manager/e3-manager-service/src/main/java/cn/e3mall/service/impl/ItemServiceImpl.java
@@ -0,0 +1,26 @@
+ package cn.e3mall.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import cn.e3mall.mapper.TbItemCatMapper;
+import cn.e3mall.mapper.TbItemMapper;
+import cn.e3mall.pojo.TbItem;
+import cn.e3mall.service.ItemService;
+
+/**
+ * 商品管理Service
+ * @author 11
+ *
+ */
+@Service
+public class ItemServiceImpl implements ItemService {
+	@Autowired
+	private TbItemMapper itemMapper;
+	@Override
+	public TbItem getItemById(long itemId) {
+		TbItem item = itemMapper.selectByPrimaryKey(itemId);
+		return item;
+	}
+
+}
